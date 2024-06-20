d2faffef240c8aad2f4fb5faee2e8b4481767754
Rahul Patel <rahul.coder91@gmail.com>
2024-06-20 13:24:41 -0700

    initial commit

---
+++ CercacorAssignment/CercacorAssignment/CercacorAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CercacorAssignmentApp.swift
+//  CercacorAssignment
+//
+//  Created by Applanding Solutions on 2024-06-20.
+//
+
+import SwiftUI
+
+@main
+struct CercacorAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
